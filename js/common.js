0c21b48b3b4154db99b4ebee1f1e9c0650cd52ad
Vasin Andrey <vofus@yandex.ru>
2015-10-11 01:44:25 +0400

    Добавлена кнопка меню с анимацией при клике по ней.

--- js/common.js
+++ js/common.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
+	$(".main_head").css("height", $(window).height());
+
+	// function heightDetect() {
+	// 	$(".main_head").css("height", $(window).height());
+	// };
+	// heightDetect();
+	// $(window).resize(function() {
+	// 	heightDetect();
+	// });
 	
-	
+	$(".sandwich, .menu_item").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
+});
+
+$(window).load(function() { 
+	$(".loader_inner").fadeOut(); 
+	$(".loader").delay(400).fadeOut("slow"); 
 });
