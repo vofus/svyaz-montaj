a70de7de87064766124fefe3b413e11252eca4ef
Vasin Andrey <vofus@yandex.ru>
2015-10-14 02:02:43 +0400

    Night commit. Added animated block-link with class .arrow_in_circle

--- js/common.js
+++ js/common.js
@@ -1,23 +1,30 @@
 $(document).ready(function() {
 
-	$(".main_head").css("height", $(window).height());
+	// $(".main_head").css("height", $(window).height());
 
-	// function heightDetect() {
-	// 	$(".main_head").css("height", $(window).height());
-	// };
-	// heightDetect();
-	// $(window).resize(function() {
-	// 	heightDetect();
-	// });
-	
-	$(".toggle_menu, .menu_item").click(function() {
+	function heightDetect() {
+		$(".main_head").css("height", $(window).height());
+	};
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});
+
+	$(".toggle_menu").click(function() {
 		$(".sandwich").toggleClass("active");
 
 		if ($(".top_menu").is(":visible")) {
 			$(".top_menu").fadeOut(600);
+			$(".top_menu li a").removeClass("fadeInUp animated");
 		} else {
 			$(".top_menu").fadeIn(600);
+			$(".top_menu li a").addClass("fadeInUp animated");
 		}
+	});
+
+	$(".top_menu ul a").click(function() {
+		$(".top_menu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
 	});
 });
 
