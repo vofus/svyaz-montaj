e98be40dc9dd59d849a550a44ed0190e94b02d5b
Vasin Andrey <vofus@yandex.ru>
2015-10-19 22:58:25 +0400

    Добавлены обработчики события касаний, для большей отзывчивости тач-интерфейса.

--- js/common.js
+++ js/common.js
@@ -10,9 +10,16 @@ $(document).ready(function() {
 		heightDetect();
 	});
 
-	$(".toggle_menu").click(function() {
-		$(".sandwich").toggleClass("active");
+	function onClickTopMenu() {
+		$(".top_menu").fadeOut(600);
+		// $(".sandwich").toggleClass("active");
+		if ($(".sandwich").is(".active")) {
+			$(".sandwich").removeClass("active");
+		}
+	}
 
+	function onClickToggleMenu () {
+		$(".sandwich").toggleClass("active");
 		if ($(".top_menu").is(":visible")) {
 			$(".top_menu").fadeOut(600);
 			$(".top_menu li a").removeClass("fadeInUp animated");
@@ -20,12 +27,52 @@ $(document).ready(function() {
 			$(".top_menu").fadeIn(600);
 			$(".top_menu li a").addClass("fadeInUp animated");
 		}
+	}
+
+	// $(".toggle_menu").click(function() {
+	// 	$(".sandwich").toggleClass("active");
+
+	// 	if ($(".top_menu").is(":visible")) {
+	// 		$(".top_menu").fadeOut(600);
+	// 		$(".top_menu li a").removeClass("fadeInUp animated");
+	// 	} else {
+	// 		$(".top_menu").fadeIn(600);
+	// 		$(".top_menu li a").addClass("fadeInUp animated");
+	// 	}
+	// });
+
+	// $(".top_menu").click(function() {
+	// 	$(".top_menu").fadeOut(600);
+	// 	// $(".sandwich").toggleClass("active");
+	// 	if ($(".sandwich").is(".active")) {
+	// 		$(".sandwich").removeClass("active");
+	// 	}
+	// });
+
+	$(".top_menu ul li a").click(function() {
+		onClickTopMenu();
+	});
+	
+	$(document).on('touchstart click', '.top_menu', function(event) {
+		if (event.type == "touchstart") {
+			onClickTopMenu();
+		} else {
+			onClickTopMenu();
+		}
+		event.stopPropagation();
+		event.preventDefault();
 	});
 
-	$(".top_menu ul a").click(function() {
-		$(".top_menu").fadeOut(600);
-		$(".sandwich").toggleClass("active");
+	$(document).on('touchstart click', '.toggle_menu', function(event) {
+		if (event.type == "touchstart") {
+			onClickToggleMenu();
+		} else {
+			onClickToggleMenu();
+		}
+		event.stopPropagation();
+		event.preventDefault();
 	});
+
 });
 
 $(window).load(function() { 
